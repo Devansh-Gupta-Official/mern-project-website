c6c1912ba9bd36afaec6a2dd7311e70073884082
Devansh-Gupta-Official <devanshcool.gupta@gmail.com>
2024-01-28 09:55:27 +0530

    adding a UsersList component

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-    return <h2> User Works!</h2>;
+    const USERS = [{id:'u1',name:'John',image:'https://isbendiyarovanezrin.github.io/RandomDogImageGenerator/img/dog.jpg',places:3}];
+
+    return <UsersList items={USERS}/>;
 };
 
 export default Users;
