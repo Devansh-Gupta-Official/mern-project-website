d96e4ccdb93aa0bd58899583833aaba470237cad
Devansh-Gupta-Official <devanshcool.gupta@gmail.com>
2024-02-07 23:19:30 +0530

    adding usereducer to manage state formwide

--- src/places/pages/NewPlaces.js
+++ src/places/pages/NewPlaces.js
@@ -1,10 +1,32 @@
-import React, {useCallback} from 'react';
+import React, {useCallback, useReducer} from 'react';
 
 import Input from '../../shared/components/FormElements/Input';
 import './NewPlace.css';
 import {VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE} from '../../shared/util/validators'
 
+const formReducer= (state,action)=>{
+    switch(action.type){
+        case'INPUT_CHANGE':
+            return {};
+        default:
+            return state;
+    }
+};
+
 const NewPlaces = () => {
+    useReducer(formReducer, {
+        inputs:{
+            title:{
+                value:'',
+                isValid:false
+            },
+            description:{
+                value:'',
+                isValid:false
+            }
+        },
+        isValid:false
+    });
     const titleInputHandler = useCallback((id,value, isValid) =>{},[]);
     const descriptionInputHandler = useCallback((id,value, isValid) =>{},[]);
     return <form className='place-form'>
